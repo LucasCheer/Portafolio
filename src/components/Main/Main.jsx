828c13c3a92efa74876ae1658eb2945c74f66c44
Lucas <cheroyonatan2@gmail.com>
2024-10-14 00:37:45 -0300

    Agrego secccion de Proyectos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,26 +1,15 @@
 import './Main.css'
+import SectionAbout from './SectionAbout/SectionAbout.jsx'
+import SectionProyects from './SectionProyects/SectionProyects.jsx'
+
 
 function Main() {
     return (
-            <section id="about-me-section" class="section">
-                <figure class="photo-about-me">  
-                </figure>
-                <div class="container-about-me">
-                    <h2 class="about-me-title">Sobre mí<span class="point">.</span></h2>
-                    <p class="about-me-text">
-                        ¡Hola! 😊 <br/>
-                        Mi nombre es <span class="resaltar">Lucas Chero</span> y soy un apasionado <span class="resaltar">Desarrollador Full-Stack</span>. Actualmente, estoy cursando la carrera de Sistemas de información en la <span class="resaltar">Universidad de Buenos Aires</span>, donde he adquirido una sólida base de conocimientos en desarrollo de software y tecnologías de la información.
-                    </p>
-                    <br/>
-                    <p class="about-me-text">
-                        Mi objetivo es escribir código que sea seguro, escalable y limpio. Me considero una persona proactiva, que siempre busca mejorar y <span class="resaltar">aprender nuevas tecnologías</span>. Disfruto colaborando en equipo, soy responsable, organizado y siempre estoy dispuesto a asumir nuevos retos.
-                    </p>
-                    <br/>
-                    <p class="about-me-text">
-                        Si te gustaría conocer más sobre mí o discutir posibles colaboraciones, no dudes en ponerte en contacto. <span class="resaltar">¡Será un placer conectar contigo!</span>
-                    </p>
-                </div>
-            </section>
+        <>
+            <SectionAbout />
+            <SectionProyects />
+        </>
+
     )
 }
 
---
+++ src/components/Main/SectionProyects/SectionProyects.jsx
@@ -0,0 +1,145 @@
+import './SectionProyects.css';
+
+function SectionProyects() {
+    return (
+        <section id="projects-section" class="section">
+            <h2 id="title-project" class="titles" >Proyectos<span class="point">.</span></h2>
+            <p  id="subtitle-project" class="subtitle">Algunos de mis proyectos...</p>
+            <div id="container-projects">
+                <article class="project-num">
+                    <figure class="photo-project">
+                        <a href="#">
+                        </a>
+                    </figure>
+
+                    <div class="info-project">
+                        <h2>Acortador de URL</h2>
+                        <div class="tablet-container">
+                            <div class="tablet">HTML</div>
+                            <div class="tablet">CSS</div>
+                        </div>
+                        <p class="descrip-project">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    </div>
+                    <div class="btn-container">
+                        <button class="btn btn-light btn-projects">Repositorio
+                        </button>
+                        <button class="btn btn-projects btn-dark">Demo</button>
+                    </div>
+                </article>
+                <article class="project-num">
+                    <figure class="photo-project">
+                        <a href="#"></a>
+                    </figure>
+                    <div class="info-project">
+                        <h2>Ladding Page</h2>
+                        <div class="tablet-container">
+                            <div class="tablet">HTML</div>
+                            <div class="tablet">CSS</div>
+                        </div>
+                        <p class="descrip-project">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    </div>
+                    <div class="btn-container">
+                        <button class="btn btn-projects btn-light">Repositorio</button>
+                        <button class="btn btn-projects btn-dark">Demo</button>
+                    </div>
+                </article>
+                <article class="project-num">
+                    <figure class="photo-project">
+                        <a href="#">
+                        </a>
+                    </figure>
+                    <div class="info-project">
+                        <h2>Advice Generator App</h2>
+                        <div class="tablet-container">
+                            <div class="tablet">HTML</div>
+                            <div class="tablet">CSS</div>
+                        </div>
+                        <p class="descrip-project">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    </div>
+                    <div class="btn-container">
+                        <button class="btn btn-projects btn-light">Repositorio</button>        
+                        <button class="btn btn-projects btn-dark">Demo</button>
+                    </div>
+                </article>
+                <article class="project-num">
+                    <figure class="photo-project">
+                        <a href="#">
+                        </a>
+                    </figure>
+                    <div class="info-project">
+                        <h2>Hein</h2>
+                        <div class="tablet-container">
+                            <div class="tablet">HTML</div>
+                            <div class="tablet">CSS</div>
+                            <div class="tablet">Sass</div>
+                            <div class="tablet">NodeJS</div>
+                        </div>
+                        <p class="descrip-project">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    </div>
+                    <div class="btn-container">
+                        <button class="btn btn-projects btn-light">Repositorio</button>
+                        <button class="btn btn-projects btn-dark">Demo</button>
+                    </div>
+                </article>
+            </div>
+            <h2 class="subtitle title-skills">Tecnologías</h2>
+            <p class="about-me-text">Lenguajes, Frameworks y Herramientas con las que me siento comodo.</p>
+            <div id="container-skills">
+                <div class="container-type front-end">
+                    <h4 class="skill-type">Frontend</h4>
+                    <div class="skill-grid">
+                        <article class="item-skill">
+                            <figcaption>HTML</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>CSS</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>JavaScript</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>Sass</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>Bootstrap</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>ReactJS</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>NextJS</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>Angular</figcaption>
+                        </article>  
+                    </div>
+                </div>
+                <div class="container-type back-end">
+                    <h4 class="skill-type">Backend</h4>
+                    <div class="skill-grid">
+                        <article class="item-skill">
+                            <figcaption>NodeJS</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>Express</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>NestJS</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>C#</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>MySQL</figcaption>
+                        </article>
+                        <article class="item-skill">
+                            <figcaption>MongoDB</figcaption>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SectionProyects
